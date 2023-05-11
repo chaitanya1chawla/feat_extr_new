2704adf80aba78e14eb24d274eafe3b9d6d3dacd
Chaitanya Chawla <chaitanya1chawla@gmail.com>
2023-05-11 16:37:34 +0200

    updated trajectoryplanning

--- trajectoryPlotting.cpp
+++ trajectoryPlotting.cpp
@@ -8,6 +8,8 @@
 #include <vector>
 #include <iostream>
 #include <cmath>
+#include <json/value.h>
+#include <fstream>
 
 using namespace AndreiUtils;
 using namespace std;
@@ -17,30 +19,35 @@ using namespace Eigen;
 int main() {
     cout << "Hello World!" << endl;
 
-    PythonInterface python;
-    double myNum[8] = {10, 20, 30, 1, 0, 3,4,5}; // input quaternion
-    Eigen::Vector4d pos(myNum[5],myNum[6],myNum[7],1.0);
+    std::ifstream demo_file("data/demonstration_2023-03-17-17-11-02_426849468.json", std::ifstream::binary);
+    Json::Value demo_data; 
+    demo_file >> demo_data;
+    cout << demo_data[0];
 
-    Eigen::MatrixXd m(4,4);
-
-    double theta = acos(myNum[0]); // CHECK - see function in script at page 31 - Rotations Quaternion
-    double cos_t = cos(theta);
-    double sin_t = sin(theta);
-    double vers_t = 1 - cos_t;
-    Eigen::Vector3d k_axis = {myNum[1],myNum[2],myNum[3]} ;
-    k_axis /= sin_t;
-
-    // CHECK - see function in script at page 28 - Transformation
-    m.col(0) = Eigen::Vector4d{k_axis[0]*k_axis[0]*vers_t + cos_t ,           k_axis[0]*k_axis[1]*vers_t + k_axis[2]* sin_t,  k_axis[0]*k_axis[2]*vers_t -
-                                                                                                                    k_axis[1]*sin_t, 0};
-    m.col(0) = Eigen::Vector4d{k_axis[0]*k_axis[1]*vers_t - k_axis[2]*sin_t , k_axis[1]*k_axis[1]*vers_t + cos_t,             k_axis[1]*k_axis[2]*vers_t +
-                                                                                                                    k_axis[0]*sin_t, 0};
-    m.col(0) = Eigen::Vector4d{k_axis[0]*k_axis[2]*vers_t + k_axis[1]*sin_t , k_axis[1]*k_axis[2]*vers_t - k_axis[1] * sin_t, k_axis[2]*k_axis[2]*vers_t +
-                                                                                                                   cos_t,             0};
-    m.col(3) = pos;
-
-    python.reInitialize("scripts.plotting", {"plot_coord"});
-    python.callFunction("plot_coord", m);
-
+//    PythonInterface python;
+//    double myNum[8] = {10, 20, 30, 1, 0, 3,4,5}; // input quaternion
+//    Eigen::Vector4d pos(myNum[5],myNum[6],myNum[7],1.0);
+//
+//    Eigen::MatrixXd m(4,4);
+//
+//    double theta = acos(myNum[0]); // CHECK - see function in script at page 31 - Rotations Quaternion
+//    double cos_t = cos(theta);
+//    double sin_t = sin(theta);
+//    double vers_t = 1 - cos_t;
+//    Eigen::Vector3d k_axis = {myNum[1],myNum[2],myNum[3]} ;
+//    k_axis /= sin_t;
+//
+//    // CHECK - see function in script at page 28 - Transformation
+//    m.col(0) = Eigen::Vector4d{k_axis[0]*k_axis[0]*vers_t + cos_t ,           k_axis[0]*k_axis[1]*vers_t + k_axis[2]* sin_t,  k_axis[0]*k_axis[2]*vers_t -
+//                                                                                                                    k_axis[1]*sin_t, 0};
+//    m.col(0) = Eigen::Vector4d{k_axis[0]*k_axis[1]*vers_t - k_axis[2]*sin_t , k_axis[1]*k_axis[1]*vers_t + cos_t,             k_axis[1]*k_axis[2]*vers_t +
+//                                                                                                                    k_axis[0]*sin_t, 0};
+//    m.col(0) = Eigen::Vector4d{k_axis[0]*k_axis[2]*vers_t + k_axis[1]*sin_t , k_axis[1]*k_axis[2]*vers_t - k_axis[1] * sin_t, k_axis[2]*k_axis[2]*vers_t +
+//                                                                                                                   cos_t,             0};
+//    m.col(3) = pos;
+//
+//    python.reInitialize("scripts.plotting", {"plot_coord"});
+//    python.callFunction("plot_coord", m);
+//
     return 0;
 }
