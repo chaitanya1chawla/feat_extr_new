375e49933574d5e7fa248c96c644439db0ecd62d
Chaitanya Chawla <chaitanya1chawla@gmail.com>
2023-05-17 19:06:36 +0200

    updated voxelExtraction.cpp

--- voxelExtraction.cpp
+++ voxelExtraction.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <fstream>
 #include <AndreiUtils/utilsJson.h>
+#include <AndreiUtils/classes/DualQuaternion.hpp>
 
 #include <pcl/point_cloud.h>
 #include <pcl/octree/octree_search.h>
@@ -15,34 +16,36 @@ using namespace Eigen;
 using namespace pcl;
 
 
-
 int main() {
     cout << "Hello World!" << endl;
 
     json data = readJsonFile("../data/demonstration_2023-05-08-13-32-14_980300108.json");
-    
-    PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+
+    PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
     // Generate pointcloud data
     cloud->width = 500;
     cloud->height = 1;
-    cloud->points.resize (cloud->width * cloud->height);
+    cloud->points.resize(cloud->width * cloud->height);
 
-    for (int i = 0; i < 400; ++i)
-    {
+    for (int i = 0; i < 400; ++i) {
         vector<double> src = data[i]["objects"]["CerealBoxKelloggsMuslixInstance"]["geometryPose"];
+        Posed q;
+        q.fromCoefficients(src);
+        auto t = q.getTranslation();
+        (*cloud)[i].x = t.x();
+        (*cloud)[i].y = t.y();
+        (*cloud)[i].z = t.z();
+        cout << t.transpose() << endl;
+    }
 
-        (*cloud)[i].x = src[5];
-        (*cloud)[i].y = src[6];
-        (*cloud)[i].z = src[7];
-    } 
+    float resolution = 0.001f;
+    resolution = 1.f;
 
-    float resolution = 128.0f;
-
-    octree::OctreePointCloud<pcl::PointXYZ> octree (resolution);
+    octree::OctreePointCloud<pcl::PointXYZ> octree(resolution);
     octree.setInputCloud(cloud);
     octree.addPointsFromInputCloud();
     octree::OctreePointCloud<pcl::PointXYZ>::AlignedPointTVector ab;
-    cout<<octree.getOccupiedVoxelCenters(ab);
-    cout<<
+    cout << octree.getOccupiedVoxelCenters(ab) << endl;
+    // cout<<
 }
 
