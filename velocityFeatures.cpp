c780dd2b57aba6e1a5d03a65faefec4fc82ba359
Chaitanya Chawla <chaitanya1chawla@gmail.com>
2023-07-04 15:58:26 +0200

    added velocityConstant check to velocityFeatures.cpp

--- velocityFeatures.cpp
+++ velocityFeatures.cpp
@@ -15,6 +15,7 @@
 
 #include <pcl/point_cloud.h>
 #include <pcl/octree/octree_pointcloud_density.h>
+#include <stack>
 
 
 using json = nlohmann::json;
@@ -27,82 +28,151 @@ using namespace pcl;
 // set here the maximum acceptable velocity -
 double const max_vel = 0.25;
 
-bool velocityLessThan(double dist, double time){
-    return dist/time < max_vel;
+int velocityLessThan(double dist, double time, int &velocityLessThanCheck, int &velocityCtr, double currentTimeStamp,
+                      double &initialTimeStamp, double &finalTimeStamp) {
+
+    // this "if" - is used when velocityLessThan returns true for the first time.
+    if (dist / time < max_vel && velocityLessThanCheck == 0) {
+        velocityCtr = 0;
+        velocityLessThanCheck = 1;
+        initialTimeStamp = currentTimeStamp;
+    }
+        // this "else if" - is used to count how many times consecutively did the velocityLessThan returned true.
+    else if (dist / time < max_vel && velocityLessThanCheck == 1) {
+        velocityCtr++;
+    }
+        // this "else if" - is used when velocityLessThan is no more true and to see final time.
+    else if (!dist / time < max_vel && velocityLessThanCheck == 1) {
+        finalTimeStamp = currentTimeStamp;
+        velocityLessThanCheck = 0;
+    }
+
+    return velocityCtr;
 }
 
-void velocityExtract()
-{
+int velocityConstant(double val, int &velocityCtr, double &initialTimeStamp, double &finalTimeStamp, int &velocityConstantCheck){
 
-    vector<nlohmann::json> data = readJsonFile("../data/demonstration_2023-06-21-18-53-52_129873292.json").get<vector<json>>();
+    // this "if" - is used when velocityConstant returns true for the first time.
+    if ((val > -0.1 || val < 0.1 ) && velocityConstantCheck == 0) {
+        velocityCtr = 0;
+        velocityConstantCheck = 1;
+        initialTimeStamp = currentTimeStamp;
+    }
+    // this "else if" - is used to count how many times consecutively did the velocityConstant returned true.
+    else if ((val > -0.1 || val < 0.1 ) && velocityConstantCheck == 1) {
+        velocityCtr++;
+    }
+    // this "else if" - is used when velocityConstant is no more true and to see final time.
+    else if (!(val > -0.1 || val < 0.1 ) && velocityConstantCheck == 1) {
+        finalTimeStamp = currentTimeStamp;
+        velocityConstantCheck = 0;
+    }
+
+    return velocityCtr;
+}
+
+int avg_queue(queue<double> q) {
+
+    int ctr = 0;
+    if (q.empty()) {
+        cout << "Error: Queue is empty" << endl;
+        return 0;
+    }
+    int sum = 0;
+    for (int i = q.front(); i <= q.back(); i++) {
+        sum += q.front();
+        q.pop();
+        ctr++;
+    }
+    return sum / ctr;
+}
+
+void velocityExtract() {
+
+    vector<nlohmann::json> data = readJsonFile(
+            "../data/demonstration_2023-06-21-18-53-52_129873292.json").get<vector<json>>();
 
     cout << "Json data size = " << data.size() << endl;
-    // to check if pose has been saved
-    int flag = 0;
     int velocityLessThanCheck = 0;
     int velocityCtr = 0;
     double initialTimeStamp = 0;
     double finalTimeStamp = 0;
     Posed q;
+    queue<double> distQueue;
+    vector<double> movingAvg;
+    int ctr = 0;
 
-    for (int i = 0; i < data.size() - 1; i++)
-    {
+    for (int i = 0; i < data.size() - 1; i++) {
 
-        if (!data[i].contains("objects") || !data[i + 1].contains("objects"))
-        {
+        if (!data[i].contains("objects") || !data[i + 1].contains("objects")) {
             continue;
         }
         auto &objects = data[i].at("objects");
-        if (!objects.contains("MilkCartonLidlInstance1") || !data[i + 1].at("objects").contains("MilkCartonLidlInstance1"))
-        {
+        if (!objects.contains("MilkCartonLidlInstance1") ||
+            !data[i + 1].at("objects").contains("MilkCartonLidlInstance1")) {
             continue;
         }
         auto &milkCarton = objects.at("MilkCartonLidlInstance1");
         q = milkCarton.at("geometryPose").get<Posed>();
-        flag++;
         auto initialPose = q.getTranslation();
         // initial timeframe
         double lastTimeStamp = data[i].at("timeStamp");
 
-        if (flag == 1)
-        {            continue;
-        }
 
-        auto currentPose = data[i + 1].at("objects").at("MilkCartonLidlInstance1").at("geometryPose").get<Posed>().getTranslation();
+        auto currentPose = data[i + 1].at("objects").at("MilkCartonLidlInstance1").at(
+                "geometryPose").get<Posed>().getTranslation();
         double currentTimeStamp = data[i + 1].at("timeStamp");
 
-        double dist = sqrt(pow(currentPose.x() - initialPose.x(), 2) 
+        double dist = sqrt(pow(currentPose.x() - initialPose.x(), 2)
                            + pow(currentPose.y() - initialPose.y(), 2)
                            + pow(currentPose.z() - initialPose.z(), 2));
         double time = currentTimeStamp - lastTimeStamp;
+        ctr++;
 
-        // this "if" - is used when velocityLessThan returns true for the first time.
-        if (velocityLessThan(dist, time) && velocityLessThanCheck == 0){
-            velocityCtr = 0;
-            velocityLessThanCheck = 1;
-            initialTimeStamp = currentTimeStamp;
-        }
-        // this "else if" - is used to count how many time consecutively did the velocityLessThan returned true.
-        else if(velocityLessThan(dist, time) && velocityLessThanCheck == 1){
-            velocityCtr++;
-        }
-        // this "else if" - is used when velocityLessThan is no more true and to see final time.
-        else if (!velocityLessThan(dist, time) && velocityLessThanCheck == 1){
-            finalTimeStamp = currentTimeStamp;
-            velocityLessThanCheck = 0;
+
+        // checking if velocity was kept less than a value in a certain time period
+        velocityCtr = velocityLessThan(dist, time, velocityLessThanCheck, velocityCtr,
+                                       currentTimeStamp, initialTimeStamp, finalTimeStamp);
+        // if velocity was kept less than max_vel for 3-4 seconds (100 frames) -
+        if (velocityCtr != 0 && velocityCtr % 50 == 0) {
+            cout << "You kept the velocity of the object less than - " << max_vel;
+            cout << " between time period - " << initialTimeStamp << " and " << finalTimeStamp << endl;
         }
 
-        // if velocity was kept less than max_vel for 3-4 seconds -
-        if(velocityCtr > 100){
-            cout << "You kept the velocity of the object less than "<< max_vel;
+
+
+        distQueue.push(dist/time);
+        if(ctr > 50){
+            movingAvg.push_back(avg_queue(distQueue));
+            distQueue.pop();
+        }
+
+
+
+    }
+
+    velocityCtr = 0;
+    initialTimeStamp = 0;
+    finalTimeStamp = 0;
+    int velocityConstantCheck = 0;
+
+    // comparing here the moving Averages -
+    for (int i = 0; i < movingAvg.size() - 1; i++) {
+
+        double val = (movingAvg.at(i+1) - movingAvg.at(i))/ movingAvg.at(i);
+        velocityCtr = velocityConstant(val, velocityCtr, initialTimeStamp, finalTimeStamp,
+                                       velocityConstantCheck);
+        if (velocityCtr != 0 && velocityCtr % 50 == 0) {
+            cout << "You kept constant velocity of -  " << max_vel;
             cout << " between time period - " << initialTimeStamp << " and " << finalTimeStamp << endl;
         }
 
     }
+
+
 }
 
-int main()
-{
+int main() {
     cout << "Hello World!" << endl;
 
     velocityExtract();
